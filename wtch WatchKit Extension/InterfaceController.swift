283530821a82b48db6c7f944d640b8438e056934
Кабинет 35 номер пк-9 <kabinet35nomerpk-9@MacBook-Pro-Kabinet-35-nomer-pk-9.local>
2022-10-27 14:41:44 +0300

    commit

---
+++ wtch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  wtch WatchKit Extension
+//
+//  Created by  Кабинет 35 номер пк-9 on 30.11.2021.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
